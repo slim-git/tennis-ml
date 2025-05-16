e39c0cac51cfc38346a8b8197e31652d05ca6df2
sghorbal <slim.ghorbal@gmail.com>
2025-05-16 09:28:17 +0200

    add a cleaning step in phase with the new data model

--- src/repository/model_data.py
+++ src/repository/model_data.py
@@ -13,14 +13,39 @@ def load_model_data(limit: Optional[int] = None) -> pd.DataFrame:
             query += f" LIMIT {limit}"
         
         data = pd.read_sql_query(query, conn)
+    
+    # Clean the data
+    data = _clean_data(data)
+
+    return data
+
+def _clean_data(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Clean the data by removing unnecessary columns and rows with null values
+    and filtering based on specific criteria.
+    """
+    # Drop unnecessary columns
+    columns_to_drop = [
+        'match_id', 'date',
+        'tournament_name', 'tournament_location',
+        'winner_name', 'loser_name',
+        'min_winner', 'min_loser',
+        'avg_winner', 'avg_loser',
+        'max_winner', 'max_loser'
+    ]
+    data.drop(columns=columns_to_drop, inplace=True, errors='ignore')
+
+    # Remove rows where 'victory_type' is not 'Completed'
+    data = data[data['victory_type'] == 'Completed']
+    data.drop(columns='victory_type', inplace=True, errors='ignore')
+    
+    # Remove rows where 'tournament_series' is not in the specified list
+    data = data[data['tournament_series'].isin(['Grand Slam', 'Masters 1000', 'Masters', 'Masters Cup', 'ATP500', 'ATP250'])]
 
     # Remove rows with null values
     data.dropna(inplace=True)
 
-    # Remove rows where 'victory_type' is not 'Completed'
-    data = data[data['victory_type'] == 'Completed']
-    
-    # Remove rows where 'tournament_series' is not in the specified list
-    data = data[data['tournament_series'].isin(['Grand Slam', 'Masters 1000', 'Masters', 'Masters Cup', 'ATP500', 'ATP250'])]
+    # Reset the index
+    data.reset_index(drop=True, inplace=True)
 
     return data
